4d2b316d3326e823eb6dbaea2e10a2d9e726d97e
NgassakiJunescyPRATH <junescy.ngassaki@pratho.com>
2024-08-19 17:07:39 +0200

    id checked

--- src/app/components/Modal/modal/modal.component.ts
+++ src/app/components/Modal/modal/modal.component.ts
@@ -47,7 +47,6 @@ export class ModalComponent implements OnInit {
       completed: this.isEditModal() ? this.currentTask()!.completed : false,
       title: this.addForm().value.title as string,
       userId,
-      id: this.isEditModal() ? this.currentTask()!.id : '',
     }
     try {
       if (this.isEditModal()){
